3ba82e2e9df6e1a24102dcd5433b3a0234a0de76
LAPTOP-VIFOVNGG\idyk <dykas@oakland.edu>
2022-01-26 15:55:08 -0500

    Revert "Added line"
    This reverts commit 44aab3885f656d502b6033ac07f946cdaa6cc548.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,7 +17,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
-        System.out.println("awesome");
     }
     
 }
